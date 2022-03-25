5e3c00279e8bae07cec2a7f0fcacb756d5a01cf5
Tariq Khan <Khantarikact@gmail.com>
2022-03-25 21:11:29 +0530

    Finish Project

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import NotFound from './pages/NotFound';
 import Navbar from './components/layouts/Navbar';
 import Footer from './components/layouts/Footer';
 import Alert from './components/layouts/Alert';
+import User from './pages/User';
 import { GithubProvider } from './context/github/GithubContext';
 import { AlertProvider } from './context/alert/AlertContext';
 
@@ -20,6 +21,7 @@ function App() {
               <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/about' element={<About />} />
+                <Route path='/user/:login' element={<User />} />
                 <Route path='/notfound' element={<NotFound />} />
                 <Route path='/*' element={<NotFound />} />
               </Routes>
